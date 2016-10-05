a250a95a57a694602bd328b27ba18497349986b0
Robin Bolscher <robinbolscher@gmail.com>
2016-10-05 21:14:27 +0200

    Basic functionality working (set target_temperature partially)

---
+++ drivers/LC13/driver.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const path = require('path');
+const ZwaveDriver = require('homey-zwavedriver');
+
+// http://www.vesternet.com/downloads/dl/file/id/196/product/1128/z_wave_danfoss_lc_13_living_connect_radiator_thermostat_manual.pdf
+
+module.exports = new ZwaveDriver(path.basename(__dirname), {
+	capabilities: {
+		measure_battery: {
+			command_class: 'COMMAND_CLASS_BATTERY',
+			command_get: 'BATTERY_GET',
+			command_report: 'BATTERY_REPORT',
+			command_report_parser: report => {
+				if (report['Battery Level'] === "battery low warning") return 1;
+				return report['Battery Level (Raw)'][0];
+			}
+		},
+		target_temperature: {
+			command_class: 'COMMAND_CLASS_THERMOSTAT_SETPOINT',
+			command_get: 'THERMOSTAT_SETPOINT_GET',
+			command_get_parser: function () {
+				return new Buffer([1]);
+			},
+			command_set: 'THERMOSTAT_SETPOINT_SET',
+			command_set_parser: function (value) {
+
+				// This should work in theory (due to 16bit signed int) but it doesn't, wrong values are sent to thermostat
+				// return new Buffer([1, 34, (Math.round(value * 2) / 2 * 10).toFixed(0)]); // Precision = 1, Scale = 0, Size = 2
+
+				// This only works with values smaller than 127 and greater than -128 (8bit signed int)
+				return new Buffer([1, 33, (Math.round(value * 2) / 2 * 10).toFixed(0)]); // Precision = 1, Scale = 0, Size = 1
+			},
+			command_report: 'THERMOSTAT_SETPOINT_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Level2')
+					&& report.Level2.hasOwnProperty('Scale')
+					&& report.Level2['Scale'] === 0
+					&& typeof report['Value'].readUIntBE(0, 2) !== 'undefined') {
+					return report['Value'].readUIntBE(0, 2) / 100;
+				}
+				return null;
+			},
+		},
+	},
+	settings: {
+
+	}
+});
