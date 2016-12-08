94edb645c4c0a549ecdd483721d98d14ecea6cfb
Robin Bolscher <robinbolscher@gmail.com>
2016-12-08 17:52:13 +0100

    Update mobile card, set step size, update homey-zwavedriver, add getOnWakeUp for BATTERY_GET

--- drivers/LC13/driver.js
+++ drivers/LC13/driver.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const path = require('path');
-const ZwaveDriver = require('homey-zwavedriver');
+const ZwaveDriver = require('node-homey-zwavedriver');
 
 // http://www.vesternet.com/downloads/dl/file/id/196/product/1128/z_wave_danfoss_lc_13_living_connect_radiator_thermostat_manual.pdf
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
 		measure_battery: {
+			getOnWakeUp: true,
 			command_class: 'COMMAND_CLASS_BATTERY',
 			command_get: 'BATTERY_GET',
 			command_report: 'BATTERY_REPORT',
@@ -37,7 +38,9 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 				};
 			},
 			command_set: 'THERMOSTAT_SETPOINT_SET',
-			command_set_parser: function (value) {
+			command_set_parser: function (value, node) {
+
+				module.exports.realtime(node.device_data, 'target_temperature', Math.round(value * 2) / 2);
 
 				// Create value buffer
 				let a = new Buffer(2);
@@ -63,7 +66,6 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 					&& report.Level2['Scale'] === 0
 					&& report.Level2['Size'] !== 'undefined'
 					&& typeof report['Value'].readUIntBE(0, report.Level2['Size']) !== 'undefined') {
-
 					return report['Value'].readUIntBE(0, report.Level2['Size']) / Math.pow(10, report.Level2['Precision']);
 				}
 				return null;
