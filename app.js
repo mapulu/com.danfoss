627069ab83d5497bf89cfcfd6a07fc670c26ef6f
robinbolscher@gmail.com <robinbolscher@gmail.com>
2017-10-02 17:29:50 +0200

    Update dependencies, minor fixes

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 'use strict';
 
 const Homey = require('homey');
+const Log = require('homey-log');
 
 class DanfossZwave extends Homey.App {
 
 	onInit() {
-
 		this.log('Danfoss Z-wave app is running...');
-
 	}
-
 }
 
 module.exports = DanfossZwave;
--- drivers/LC13/device.js
+++ drivers/LC13/device.js
@@ -1,15 +1,13 @@
 'use strict';
 
-const Homey = require('homey');
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-class Danfoss_LC extends ZwaveDevice {
+class DanfossLC extends ZwaveDevice {
 	onMeshInit() {
-		//this.enableDebug();
-		//this.printNode();
 		this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT');
 		this.registerCapability('alarm_battery', 'BATTERY');
 		this.registerCapability('measure_battery', 'BATTERY');
 	}
 }
-module.exports = Danfoss_LC;
+
+module.exports = DanfossLC;
